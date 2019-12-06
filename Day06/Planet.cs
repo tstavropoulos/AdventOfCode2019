6fc1e8cf4a9ca86bd9f1246054bbbc969b6566cb
Trevor Stavropoulos <Trevor.Stavropoulos@gmail.com>
2019-12-06 10:05:24 -0800

    Small cleanup of Day 6

---
+++ Day06/Planet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day06
+{
+    public class Planet
+    {
+        public string Name { get; }
+
+        public Planet parent = null;
+        public List<Planet> children = new List<Planet>();
+
+        public int Distance { get; set; } = int.MaxValue;
+
+        public int Tier
+        {
+            get
+            {
+                if (parent == null)
+                {
+                    return 0;
+                }
+
+                return parent.Tier + 1;
+            }
+        }
+
+        public Planet(string name)
+        {
+            Name = name;
+        }
+
+        public void SetParent(Planet parent)
+        {
+            if (this.parent != null)
+            {
+                throw new Exception();
+            }
+
+            this.parent = parent;
+        }
+
+        public void AddChild(Planet child)
+        {
+            children.Add(child);
+        }
+
+        public IEnumerable<Planet> Connections()
+        {
+            if (parent != null)
+            {
+                yield return parent;
+            }
+
+            foreach (Planet child in children)
+            {
+                yield return child;
+            }
+        }
+    }
+}
--- Day06/Program.cs
+++ Day06/Program.cs
@@ -13,13 +13,13 @@ namespace Day06
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 6");
+            Console.WriteLine("Day 6 - Universal Orbit Map");
             Console.WriteLine("Star 1");
             Console.WriteLine();
 
             string[] lines = File.ReadAllLines(inputFile);
 
-            foreach(string line in lines)
+            foreach (string line in lines)
             {
                 Planet parent = GetPlanet(line.Substring(0, 3));
                 Planet child = GetPlanet(line.Substring(4, 3));
@@ -28,13 +28,9 @@ namespace Day06
                 child.SetParent(parent);
             }
 
+            int output1 = planets.Values.Select(x => x.Tier).Sum();
 
-
-            int output1 = planets.Values.Select(x=>x.Tier).Sum();
-
-
-
-            Console.WriteLine($"The answer is: {output1}");
+            Console.WriteLine($"The cumulative depth is: {output1}");
 
             Console.WriteLine();
             Console.WriteLine("Star 2");
@@ -62,7 +58,7 @@ namespace Day06
 
                 int newDistance = current.Distance + 1;
 
-                foreach(Planet next in current.Connections())
+                foreach (Planet next in current.Connections())
                 {
                     if (next.Distance > newDistance)
                     {
@@ -72,10 +68,7 @@ namespace Day06
                 }
             }
 
-
-
-            Console.WriteLine($"The answer is: {end.Distance}");
-
+            Console.WriteLine($"The distance from you to santa is: {end.Distance}");
 
             Console.WriteLine();
             Console.ReadKey();
@@ -89,62 +82,6 @@ namespace Day06
             }
 
             return planets[name];
-        }
-    }
-
-    public class Planet
-    {
-        public string Name { get; }
-
-        public Planet parent = null;
-        public List<Planet> children = new List<Planet>();
-
-        public int Distance { get; set; } = int.MaxValue;
-
-        public int Tier
-        {
-            get
-            {
-                if (parent == null)
-                {
-                    return 0;
-                }
-
-                return parent.Tier + 1;
-            }
-        }
-
-        public Planet(string name)
-        {
-            Name = name;
-        }
-
-        public void SetParent(Planet parent)
-        {
-            if (this.parent != null)
-            {
-                throw new Exception();
-            }
-
-            this.parent = parent;
-        }
-
-        public void AddChild(Planet child)
-        {
-            children.Add(child);
-        }
-
-        public IEnumerable<Planet> Connections()
-        {
-            if (parent != null)
-            {
-                yield return parent;
-            }
-
-            foreach (Planet child in children)
-            {
-                yield return child;
-            }
         }
     }
 }
