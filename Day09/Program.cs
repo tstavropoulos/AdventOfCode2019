61ab9c976a504fdb5e140dc6478da14c165b922d
Trevor Stavropoulos <Trevor.Stavropoulos@gmail.com>
2019-12-09 08:45:28 -0800

    Day 9 cleanup

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -12,38 +12,33 @@ namespace Day09
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 9");
+            Console.WriteLine("Day 9 - Sensor Boost");
             Console.WriteLine("Star 1");
             Console.WriteLine();
 
-
             string line = File.ReadAllText(inputFile);
-            //string line = "104,1125899906842624,99";
 
             long[] regs = line.Split(",").Select(long.Parse).ToArray();
 
-            IntCode machine = new IntCode("Star 1", regs, new long[] { 1 }, output: Console.WriteLine);
+            IntCode machine = new IntCode(
+                name: "Star 1",
+                regs: regs,
+                fixedInputs: new long[] { 1 },
+                output: x => Console.WriteLine($"Star 1 output: {x}"));
 
             machine.Run().Wait();
-
-            Console.WriteLine($"The answer is: {machine.lastOutput}");
 
             Console.WriteLine();
             Console.WriteLine("Star 2");
             Console.WriteLine();
 
+            IntCode machine2 = new IntCode(
+                name: "Star 2",
+                regs: regs,
+                fixedInputs: new long[] { 2 },
+                output: x => Console.WriteLine($"Star 2 output: {x}"));
 
-            int output2 = 0;
-            
-
-            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
-            sw.Start();
-            IntCode machine2 = new IntCode("Star 2", regs, new long[] { 2 }, output: Console.WriteLine);
             machine2.Run().Wait();
-            sw.Stop();
-
-            Console.WriteLine($"Took: {sw.Elapsed.TotalMilliseconds} ms");
-
 
             Console.WriteLine();
             Console.ReadKey();
